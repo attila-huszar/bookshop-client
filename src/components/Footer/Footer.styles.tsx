8e27a282e439c36395a51ef788a42243fed2592f
Attila Huszar <attila.huszar@nixs.com>
2024-03-28 11:49:58 +0100

    add footer

--- src/components/Footer/Footer.styles.tsx
+++ src/components/Footer/Footer.styles.tsx
@@ -3,7 +3,78 @@ import styled from 'styled-components'
 export const StyledFooter = styled.footer`
   position: absolute;
   bottom: 0;
-  height: 5rem;
+  display: grid;
+  grid-template-rows: 1fr 3.5rem;
+  grid-template-columns: 1fr 1fr;
+  height: 25rem;
   width: 100%;
-  background: var(--textGreyColor);
+  max-width: 90rem;
+  padding: 0 6.25rem;
+`
+
+export const StDiv1 = styled.div`
+  grid-area: 1 / 1 / 2 / 2;
+  padding: 3rem 0;
+`
+
+export const StDiv2 = styled.div`
+  grid-area: 1 / 2 / 2 / 3;
+  padding: 4.75rem 2.5rem;
+`
+
+export const StDiv3 = styled.div`
+  grid-area: 2 / 1 / 3 / 3;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-top: 1px #0d08421a solid;
+`
+
+export const NavList = styled.ul`
+  display: flex;
+  gap: 2rem;
+  list-style-type: none;
+`
+
+export const SocialsList = styled.div`
+  display: flex;
+  gap: 3rem;
+`
+
+export const Spacer = styled.div`
+  height: 3rem;
+`
+
+export const SubscribeForm = styled.form`
+  display: flex;
+  margin-top: 0.5rem;
+  border: 2px solid var(--generalColor);
+  border-radius: 10px;
+  overflow: hidden;
+
+  input {
+    flex-grow: 2;
+    padding: 0.5rem 2rem;
+    border: none;
+    font-size: 1rem;
+    font-weight: 700;
+    color: #6c6c6c;
+  }
+
+  input:focus {
+    outline: none;
+  }
+
+  form:focus-within {
+    outline: 2px solid var(--favoriteColor);
+  }
+
+  button {
+    padding: 0.5rem 2rem;
+    border: none;
+    font-size: 1rem;
+    font-weight: 700;
+    color: #fff;
+    background-color: var(--generalColor);
+  }
 `
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,56 @@
-import { StyledFooter } from './Footer.styles'
+import {
+  StyledFooter,
+  StDiv1,
+  StDiv2,
+  StDiv3,
+  NavList,
+  SocialsList,
+  Spacer,
+  SubscribeForm,
+} from './Footer.styles'
+import logo from '../../assets/svg/logo.svg'
+import instagramIcon from '../../assets/svg/instagram.svg'
+import googleIcon from '../../assets/svg/google.svg'
+import fbIcon from '../../assets/svg/facebook.svg'
 
 export function Footer() {
-  return <StyledFooter />
+  return (
+    <StyledFooter>
+      <StDiv1>
+        <img src={logo} alt="logo" />
+        <Spacer />
+        <NavList>
+          <li>About</li>
+          <li>Features</li>
+          <li>Pricing</li>
+          <li>Gallery</li>
+          <li>Team</li>
+        </NavList>
+      </StDiv1>
+      <StDiv2>
+        <p>
+          Subscribe to stay tuned for new product and latest updates. Let's do
+          it!
+        </p>
+        <Spacer />
+        <SubscribeForm>
+          <input type="text" placeholder="Enter your email address" />
+          <button type="button">Subscribe</button>
+        </SubscribeForm>
+      </StDiv2>
+      <StDiv3>
+        <NavList>
+          <li>Privacy Policy</li>
+          <li>Terms of Use</li>
+          <li>Sales and Refunds</li>
+          <li>Legal</li>
+        </NavList>
+        <SocialsList>
+          <img src={instagramIcon} alt="instagram"></img>
+          <img src={googleIcon} alt="google"></img>
+          <img src={fbIcon} alt="facebook"></img>
+        </SocialsList>
+      </StDiv3>
+    </StyledFooter>
+  )
 }
