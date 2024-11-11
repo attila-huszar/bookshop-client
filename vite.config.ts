c5aa4809ccabf9c8ea7358694581b79a9fbcebc6
Attila Huszár <attila.huszar@outlook.com>
2024-11-11 20:35:55 +0100

    add rollup visualizer config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, loadEnv } from 'vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 import path from 'path'
@@ -7,7 +8,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
 
   return {
-    plugins: [react(), svgr()],
+    plugins: [react(), svgr(), visualizer()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
