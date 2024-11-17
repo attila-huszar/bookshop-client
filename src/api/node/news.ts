e6f99d35efdafb57825250ed955264ce4de36f60
Attila Huszár <attila.huszar@outlook.com>
2024-11-17 15:38:05 +0100

    upd news fetch

--- src/api/node/news.ts
+++ src/api/node/news.ts
@@ -1,15 +1,11 @@
-import axios from 'axios'
-import { jsonServerPath, PATH } from '@/constants'
+import { api } from './api'
+import { PATH } from '@/constants'
 import { handleErrors } from '@/errors'
 import { INews } from '@/interfaces'
 
 export const getNews = async (): Promise<INews[]> => {
   try {
-    const { data }: { data: INews[] } = await axios.get(
-      `${jsonServerPath}/${PATH.news}`,
-    )
-
-    return data
+    return api.get(`${PATH.news}`).json()
   } catch (error) {
     throw handleErrors(error, 'Unable to get news')
   }
