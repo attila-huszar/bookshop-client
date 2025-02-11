e11aabc99e2447ec5907846a465f1cd09648406e
Attila Huszar <attila.huszar@nixs.com>
2025-02-11 12:56:31 +0100

    refactor: remove Cloudinary configuration and update dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,14 @@
-import * as Sentry from '@sentry/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './store'
-import { serverUrl } from './constants'
 import App from './App.tsx'
-
-Sentry.init({
-  dsn: 'https://afe5bb4ce6b2b89cb11447bdfcc66d40@o4508474170802176.ingest.de.sentry.io/4508529143447632',
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
-  ],
-  tracesSampleRate: 1.0,
-  tracePropagationTargets: [serverUrl],
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-})
 
 const container = document.getElementById('root')!
 const root = createRoot(container, {
-  onUncaughtError: Sentry.reactErrorHandler(),
-  onCaughtError: Sentry.reactErrorHandler(),
-  onRecoverableError: Sentry.reactErrorHandler(),
+  onUncaughtError: console.error,
+  onCaughtError: console.warn,
+  onRecoverableError: console.warn,
 })
 
 root.render(
