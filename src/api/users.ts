dc91ceea7be6ad412ce61d3a593f3f5088823456
Attila Huszár <105883495+attila-huszar@users.noreply.github.com>
2025-02-20 11:10:21 +0100

    Update users.ts

--- src/api/users.ts
+++ src/api/users.ts
@@ -6,7 +6,7 @@ import type { User, UserUpdate } from '@/types'
 export const retrieveAuthTokens = async (): Promise<{
   accessToken: string
 }> => {
-  return baseRequest.get(PATH.SERVER.users.refresh).json()
+  return baseRequest.get(PATH.SERVER.users.refresh, { credentials: 'include' }).json()
 }
 
 export const getUserProfile = async (): Promise<User> => {
